4b52e87f7839a80afd896e746cfa27bfe9afb92d
Andrey Kabylin <andrey@kabylin.ru>
2019-04-14 21:13:38 +0700

    clean up

--- app/src/main/java/ru/kabylin/andrey/telegramcontest/chart/ChartSolverImpl.java
+++ app/src/main/java/ru/kabylin/andrey/telegramcontest/chart/ChartSolverImpl.java
@@ -90,7 +90,13 @@ public class ChartSolverImpl implements ChartSolver {
 
     @Override
     public void calculate2YMinimapPoints(Rect rect) {
-        chartState.minimapRect = rect;
+        if (chartState.minimapRect == null) {
+            chartState.minimapRect = rect;
+            // it'll clamp automatically to border
+            setMinimapPosition(9999999);
+        } else {
+            chartState.minimapRect = rect;
+        }
 
         for (final ChartData chartData : chartState.charts) {
             final float max = findMaxByY(chartData.originalData).y;
